52c17ab21d0e3eced9d5374c67fda854566d3fa1
Jerry Fernandez <gmfernandez@cpp.edu>
2018-11-21 21:42:00 -0800

    Added a few minor functions.

--- tictactoe.lisp
+++ tictactoe.lisp
@@ -2,27 +2,45 @@
 
 ;useful for defining array from 0 to size
 #| (defun printlong(size)
-		(loop for x from 0 to (- size 1) do
+		(loop for index from 0 to (- size 1) do
 		(format t "~a " x))) |#
 
 (defun set-board()
 	(setf my_board (make-array '(9)))
-	(loop for x from 0 to 8 do
-    (setf (aref my_board x) (+ x 1))))
+	(loop for index from 0 to 8 do
+    (setf (aref my_board index) (+ index 1))))
 	
 (defun print-board()
-	(loop for x from 0 to 8 do
-		(format t "~a" (aref my_board x))))
+	(loop for index from 0 to 8 do
+		(format t "~a" (aref my_board index))))
 
+(defun reset-board()
+	(loop for index from 0 to 8 do
+    (setf (aref my_board index) (+ index 1))))
 
+(defun empty-board()
+	(loop for index from 0 to 8 do
+    (setf (aref my_board index) " ")))
 
 (defun print-2D()
-	(loop for x from 0 to 2 do
-		(loop for y from 0 to 2 do
-			(format t "   ~a   |" (aref my_board (+ (* x 3) y))))
+	(loop for index from 0 to 2 do
+		(loop for inner_index from 0 to 2 do
+			(format t "   ~a   |" (aref my_board (+ (* index 3) inner_index))))
 		(terpri)
 		(format t " ______________________ ~%")
 		(terpri)))
+
+(defun print-square(index)
+	(format t "~a" (aref my_board (- index 1))))
+
+(defun change-square(index token)
+	(setf (aref my_board (- index 1)) token))
+
+;;choose player token
+;;change player
+;;test for winning conditions
+;;set winner
+;;minmax
 
 (set-board)
 (print-2D)
