aed48e6eb51a46e08caceb79abb6efb591f356f3
Jerry Fernandez <gmfernandez@cpp.edu>
2018-11-22 02:40:13 -0800

    Added a  basic class.

--- tictactoe.lisp
+++ tictactoe.lisp
@@ -1,9 +1,23 @@
 #| Tic Tac Toe Board Formatting |#
 
+(defclass player () ;define a class player
+  ((my_token 
+  	:reader my_token 
+  	:initarg :my_token) ;initial value is the parameter given during initialization 
+   ;defines that variable "win_status" is a read & write 
+   (win_status 
+   	:accessor win_status 
+   	:initform "false"))) ;inital value is 0 votes
+
+;store instances of player objects in global dynamic variables
+(defvar player_one (make-instance 'player :my_token "X"));
+(defvar player_two (make-instance 'player :my_token "O")); 
+
+
 ;useful for defining array from 0 to size
-#| (defun printlong(size)
+(defun printlong(size)
 		(loop for index from 0 to (- size 1) do
-		(format t "~a " x))) |#
+		(format t "~a " index)))
 
 (defun set-board()
 	(setf my_board (make-array '(9)))
