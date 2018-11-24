653030b564e4a36702ab45499c8545c1d49773be
Jerry Fernandez <gmfernandez@cpp.edu>
2018-11-24 03:21:00 -0800

    A few small functions regarding score and game-status added, plus I organized the code. Need to start coding game-menu logic and then work on minmax.

--- tictactoe.lisp
+++ tictactoe.lisp
@@ -1,42 +1,17 @@
 #| Player Object Functions |#
+;-------------------------
 (defclass player () ;define a class player
   ((my_token 
   	:reader my_token 
-  	:initarg :my_token) ;initial value is the parameter given during initialization 
+  	:initarg :my_token))) ;initial value is the parameter given during initialization 
    ;defines that variable "win_status" is a read & write 
-   (win_status 
-   	:accessor win_status 
-   	:initform "false"))) ;inital value is 0 votes
+;inital value is 0 votes
+
+(defvar *players* '()) ;defines global-variable players as non-persistent list
 
 ;store instances of player objects in global dynamic variables
 (defvar player_x (make-instance 'player :my_token "X"));
 (defvar player_o (make-instance 'player :my_token "O")); 
-
-(defvar *players* '()) ;defines global-variable players as non-persistent list
-
-;defines turns, starts with Player 1 ie 0
-(defvar player-turn 0)
-
-(defun change-turn ()
-	(setf player-turn (mod (+ player-turn 1) 2)))
-;used to reset game
-(defun reset-turn()
-	(setf player-turn 0))
-
-;check whose turn it is
-(defun whose-turn()
-	(if (= (+ player-turn 0) 0) 
-		(format t "Player 1's Turn") 
-		(format t "Player 2's Turn")))
-
-;return a turn value based on whose turn it is
-(defun turn-value()
-	(if (= (+ player-turn 0) 0) 0 1))
-
-
-;hacky way to access player-turn value using s-expression rules
-;for conditional statements
-(+ player-turn 0)
 
 ;sets Player 1 as "O"
 (defun setO()
@@ -48,11 +23,6 @@
 	(push player_o *players*)
 	(push player_x *players*))
 
-;Clears out players for the next game
-(defun reset-players()
-	(pop *players*)
-	(pop *players*))
-
 ;print particular token in players 
 (defun print-token(index)
 	(format t "~a" (my_token (nth index *players*))))
@@ -61,6 +31,44 @@
 (defun get-token(index)
 	(my_token (nth index *players*)))
 
+;-------------------------
+#| END Player Object Functions |#
+
+
+#| Turn Based Functions |#
+;-------------------------
+;defines turns, starts with Player 1 ie 0
+(defvar player-turn 0)
+
+(defun change-turn ()
+	(setf player-turn (mod (+ player-turn 1) 2)))
+
+;check whose turn it is
+(defun whose-turn()
+	(if (= (+ player-turn 0) 0) 
+		(format t "Player 1's Turn") 
+		(format t "Player 2's Turn")))
+
+;return a turn value based on whose turn it is
+(defun turn-value()
+	(if (= (+ player-turn 0) 0) 0 1))
+
+(defun whose-turn()
+	(if (= (+ player-turn 0) 0) 
+		(format t "Player 1's Turn") 
+		(format t "Player 2's Turn")))
+;-------------------------
+#| END Turn Based Functions |#
+
+
+
+#| Scoring Game Functions |#
+;-------------------------
+;defines game-status variable
+(defvar is-game-over "false")
+
+(defun change-status()
+	(setf is-game-over "true")
 
 
 ;creates an array to keep track of score based
@@ -87,30 +95,33 @@
 ;;go through scores and +5 or -5 if the index is a match
 ;; for current index use aref to change it then pop temp_scores
 ;;continue until temp_scores is empty
-(format t "~a" (aref scores index))
+;(format t "~a" (aref scores index))
+;TO BE WRITTEN!
 
 ;checks scores for winner
-(defun game-over()
+(defun game-check()
 	(loop for index from 0 to 7 do
 		(if (= (aref scores index) 15) 
-			(format t "Player One Wins!")
+			(one-wins)
 			(format t ""))
 		(if (= (aref scores index) -15) 
-			(format t "Player Two Wins!" )
+			(two-wins)
 			(format t ""))))
 
-(defun whose-turn()
-	(if (= (+ player-turn 0) 0) 
-		(format t "Player 1's Turn") 
-		(format t "Player 2's Turn")))
+(defun one-wins()
+	(format t "Player One Wins!")
+	(change-status))
+
+(defun two-wins()
+	(format t "Player Two Wins!")
+	(change-status))
+
+;-------------------------
+#| END Scoring Game Functions |#
 
 
 #| Tic Tac Toe Board Formatting |#
-
-;useful for defining array from 0 to size, not used here -->future connect four
-; (defun printlong(size)
-; 		(loop for index from 0 to (- size 1) do
-; 		(format t "~a " index)))
+;-------------------------
 
 (defun set-board()
 	(setf my_board (make-array '(9)))
@@ -129,6 +140,7 @@
 	(loop for index from 0 to 8 do
     (setf (aref my_board index) " ")))
 
+;use this for tic-tac-toe display
 (defun print-2D()
 	(loop for index from 0 to 2 do
 		(loop for inner_index from 0 to 2 do
@@ -140,20 +152,29 @@
 (defun print-square(index)
 	(format t "~a" (aref my_board (- index 1))))
 
+;Method used to add tokens to board
 (defun change-square(index token)
 	(setf (aref my_board (- index 1)) token))
+;-------------------------
+#| END Tic Tac Toe Board Formatting |#
 
 
-;;test for winning conditions
-;;set winner
-;;minmax
+#| Game Reset Functions |#
+;-------------------------
+;Clears out players for the next game
+(defun reset-players()
+	(pop *players*)
+	(pop *players*))
+	
+;used to reset game
+(defun reset-turn()
+	(setf player-turn 0))
+;-------------------------
+#| END Game Reset Functions |#
 
-(score-board)
-(seto)
 
-; (print-board)
-
-;accesses an object in players
- ;(format t "~a" (WIN_STATUS (first *players*)))
-
-
+#| Game Play Functions |#
+;-------------------------
+;Looped menu method
+;-------------------------
+#| END Game Play Functions |#
