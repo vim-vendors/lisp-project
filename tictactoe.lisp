a24a7fc598ebe223b0b71d66f1567b6d9c0bc4f4
Jerry Fernandez <gmfernandez@cpp.edu>
2018-11-24 16:01:23 -0800

    Added scoring method. Now need to tie everything together with looping menu, then add minmax.

--- tictactoe.lisp
+++ tictactoe.lisp
@@ -42,16 +42,11 @@
 (defun change-turn ()
 	(setf player-turn (mod (+ player-turn 1) 2)))
 
-;check whose turn it is
-(defun whose-turn()
-	(if (= (+ player-turn 0) 0) 
-		(format t "Player 1's Turn") 
-		(format t "Player 2's Turn")))
-
 ;return a turn value based on whose turn it is
 (defun turn-value()
 	(if (= (+ player-turn 0) 0) 0 1))
 
+;used primarily for testing
 (defun whose-turn()
 	(if (= (+ player-turn 0) 0) 
 		(format t "Player 1's Turn") 
@@ -63,17 +58,18 @@
 
 #| Scoring Game Functions |#
 ;-------------------------
+
 ;defines game-status variable
 (defvar is-game-over "false")
 
+;change boolean to indicate the game is over
 (defun change-status()
-	(setf is-game-over "true")
-
+	(setf is-game-over "true"))
 
 ;creates an array to keep track of score based
 ;on the 8 possible win poitions in tic tac toe
 ;first to get to +15 or -15 wins
-(defun score-board()
+(defun create-scores()
 	(setf scores (make-array 8 
 				:element-type 'integer
 				:initial-element 0)))
@@ -89,13 +85,31 @@
 (defun score-value()
 	(if (= (+ player-turn 0) 0) 5 -5))
 
-;;create copy of new_scores --> temp_scores
-;;assign variable +5 or -5 to assign to new_scores list of indexes
-;;go through scores and +5 or -5 if the index is a match
-;; for current index use aref to change it then pop temp_scores
-;;continue until temp_scores is empty
-;(format t "~a" (aref scores index))
-;TO BE WRITTEN!
+
+;function to tabulate scores based on a given list
+(defun scoring (list)
+	(let ((temp)))
+	;;create copy of list of scores to update
+	(setq temp (copy-tree list))
+	;;go through scores and +5 or -5 if the index is a match
+	(loop for index in temp
+		do (add-score index (score-value))))
+
+;switch case that assigns various scores based on user choice
+;each choice represents a square, each list passed to scoring
+;represents the possible paths to victory for either player
+(defun user-score(choice)
+	(case choice
+		(1 (scoring (write (list 0 3 7))))
+		(2 (scoring (write (list 0 4))))
+		(3 (scoring (write (list 0 5 6)))) 
+		(4 (scoring (write (list 1 3))))
+		(5 (scoring (write (list 1 4 6))))
+		(6 (scoring (write (list 1 5))))
+		(7 (scoring (write (list 2 3 6))))
+		(8 (scoring (write (list 2 4))))
+		(9 (scoring (write (list 2 5 7))))
+		))
 
 ;checks scores for winner
 (defun game-check()
@@ -122,22 +136,22 @@
 #| Tic Tac Toe Board Formatting |#
 ;-------------------------
 
+;create an array of size 9 to represent indexes 1-9 in a tic tac toe board
+;for display purposes only, actual score is tracked in scores array
 (defun set-board()
 	(setf my_board (make-array '(9)))
 	(loop for index from 0 to 8 do
     (setf (aref my_board index) (+ index 1))))
 	
+;print "board" array as 1d array - for testing purposes
 (defun print-board()
 	(loop for index from 0 to 8 do
 		(format t "~a" (aref my_board index))))
 
+;standard board reset with indexes, used for testing
 (defun reset-board()
 	(loop for index from 0 to 8 do
     (setf (aref my_board index) (+ index 1))))
-
-(defun empty-board()
-	(loop for index from 0 to 8 do
-    (setf (aref my_board index) " ")))
 
 ;use this for tic-tac-toe display
 (defun print-2D()
@@ -148,12 +162,15 @@
 		(format t " ______________________ ~%")
 		(terpri)))
 
+;test code to print a particular square
 (defun print-square(index)
 	(format t "~a" (aref my_board (- index 1))))
 
-;Method used to add tokens to board
+;Method used to add Player tokens to board, parameters 
+;are the index of the board array and the identity of the player provided by turn-value
 (defun change-square(index token)
 	(setf (aref my_board (- index 1)) token))
+
 ;-------------------------
 #| END Tic Tac Toe Board Formatting |#
 
@@ -165,15 +182,41 @@
 	(pop *players*)
 	(pop *players*))
 	
-;used to reset game
+;used to reset turns
 (defun reset-turn()
 	(setf player-turn 0))
+
+;used to reset the board to zero
+(defun empty-board()
+	(loop for index from 0 to 8 do
+    (setf (aref my_board index) " ")))
+
+;reset game status for a new game
+(defun reset-game-status ()
+	(setf is-game-over "false"))
+
+;reset scores
+(defun reset-scores()
+	(loop for index from 0 to 7 do
+	(setf (aref scores index) 0)))
+
 ;-------------------------
 #| END Game Reset Functions |#
 
 
 #| Game Play Functions |#
 ;-------------------------
+
+;master game reset
+(defun game-reset()
+	(reset-players)
+	(reset-turn)
+	(empty-board)
+	(reset-game-status)
+	(reset-scores))
 ;Looped menu method
 ;-------------------------
 #| END Game Play Functions |#
+
+
+
